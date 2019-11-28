23507c10dc11af85b82f16c0dd48aeebc12fd033
Oksana Kabarivska <oksana.kabarivska@continuum.net>
2019-11-28 13:46:08 +0200

    add homework

---
+++ HomeWork1.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class HomeWork1 {
+
+    public static void main(String[] args) {
+        Random number = new Random();
+        byte firstByte = 50;
+        byte secondByte = 100;
+        short firstShort = (short) number.nextInt(Short.MAX_VALUE);
+        short secondShort = (short) number.nextInt(Short.MAX_VALUE);
+        double firstDouble = number.nextDouble();
+        double secondDouble = number.nextDouble();
+        float firstFloat = number.nextFloat();
+        float secondFloat = number.nextFloat();
+        long firstLong = number.nextLong();
+        long secondLong = number.nextLong();
+
+        System.out.println(firstByte + "+" + secondByte + "=" + (firstByte + secondByte));
+        System.out.println(firstShort + "-" + secondShort + "=" + (firstShort - secondShort));
+        System.out.println(firstDouble + "*" + secondDouble + "=" + firstDouble * secondDouble);
+        System.out.println(firstFloat + "/" + secondFloat + "=" + firstFloat / secondFloat);
+        System.out.println(firstLong + "+" + secondLong + "=" + (firstLong + secondLong));
+    }
+}
