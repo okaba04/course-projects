507786dde61d313ce61e4f8d5565818de8d075d4
Oksana Kabarivska <58294194+okaba04@users.noreply.github.com>
2019-12-02 11:34:48 +0200

    rename methods according convention

--- src/HomeWork2Arrays.java
+++ src/HomeWork2Arrays.java
@@ -7,18 +7,18 @@ public class HomeWork2Arrays {
     public static void main(String[] args) {
         Integer[] arrayObjects = {2, 3, 1, 7, 11};
         int[] array = {3, 3, 2, 3, 1, -4, 2, 8, -3};
-        arraySorting(arrayObjects);
-        arrayAvg(array);
-        arraySumPositiveValues(array);
-        duplicationReplacement(array);
+        sortArrayDescOrder(arrayObjects);
+        calculateArrayAvg(array);
+        calculateArrayPositiveValuesSum(array);
+        replaceArrayDuplications(array);
     }
 
-    private static void arraySorting(Integer[] array) {
+    private static void sortArrayDescOrder(Integer[] array) {
         Arrays.sort(array, Collections.reverseOrder());
         System.out.println(Arrays.toString(array));
     }
 
-    private static void arraySumPositiveValues(int[] array) {
+    private static void calculateArrayPositiveValuesSum(int[] array) {
         int sum = 0;
         for (int number : array) {
             if (number > 0)
@@ -27,7 +27,7 @@ public class HomeWork2Arrays {
         System.out.println(sum);
     }
 
-    private static void arrayAvg(int[] array) {
+    private static void calculateArrayAvg(int[] array) {
         DecimalFormat df = new DecimalFormat("####0.00");
         double sum = 0;
         for (int number : array) {
@@ -37,7 +37,7 @@ public class HomeWork2Arrays {
         System.out.println(df.format(avg));
     }
 
-    private static void duplicationReplacement(int[] array) {
+    private static void replaceArrayDuplications(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int k = i + 1; k < array.length; k++) {
                 if (array[i] == array[k]) {
