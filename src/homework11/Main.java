d7e1ea9acf30aaf982e53dbe43016c87df020cd2
okaba04 <oksana.tsvihun@gmail.com>
2020-01-15 23:12:16 +0200

    add OutOfMemoryEmulator

---
+++ src/homework11/Main.java
@@ -0,0 +1,7 @@
+package homework11;
+
+public class Main {
+    public static void main(String[] args) {
+        new OutOfMemorySimulator().emulateException();
+    }
+}
---
+++ src/homework11/OutOfMemorySimulator.java
@@ -0,0 +1,7 @@
+package homework11;
+
+public class OutOfMemorySimulator {
+    public void emulateException() {
+        Float[] array = new Float[10000 * 100000];
+    }
+}
